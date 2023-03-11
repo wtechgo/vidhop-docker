6cc48eb2ab4d0367a7776c38c497ae792bf4f2de
wtechgo <wtechgo@proton.me>
2023-03-11 06:58:00 +0100

    Remove accidentally committed files.
    Signed-off-by: wtechgo <wtechgo@proton.me>

--- docs/install_vidhop_scripts.ps1
+++
@@ -1,5 +0,0 @@
-%userprofile%\Documents\PowerShell\
-%userprofile%\Documents\WindowsPowerShell\
-
-%userprofile%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
-%userprofile%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1

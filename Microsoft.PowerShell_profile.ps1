d4e44d450670f69e087e6fa16aa4361f9a578b58
wtechgo <wtechgo@proton.me>
2023-08-14 12:18:50 +0200

    Improve Start-Vidhop in Powershell_profile.ps1
    Signed-off-by: wtechgo <wtechgo@proton.me>

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,9 @@
-# Type the following command to create the file:  
+# remove $HOME/videos from fvidf in vidhop-docker
+
+# Type the following command to create the file:
 # `New-item –type file –force $profile`
 #
-# A file Microsoft.PowerShell_profile.ps1 will be created in 
+# A file Microsoft.PowerShell_profile.ps1 will be created in
 # %userprofile%\Documents\WindowsPowerShell\ for PowerShell 5 and older or
 # %userprofile%\Documents\PowerShell\ for PowerShell 6 Core (this folder will be automatically created).
 #
@@ -9,6 +11,8 @@
 #
 # Now when you run your powershell console, Microsoft.PowerShell_profile.ps1 will be triggered.
 
+
+# $vidhop_dir = "//c/Users/<YOUR_USERNAME>/Videos/Vidhop"
 
 Function Prompt {"$(Get-Location) $ "}
 
@@ -21,17 +25,21 @@ Function Remove-Directory([string]$path) {
 }
 
 Function Start-Vidhop() {
-     if (-Not (Test-Path $PWD/media) -And -Not (Test-Path $PWD/vidhop/config/.bash_history)) {
-          Write-Output "could not find /media or /vidhop/config/.bash_history"
-          Write-Output "you are not inside the vidhop-docker directory"
-          Write-Output "navigate to the vidhop-docker directory and try again"
-          return
-     }
      echo "clearing running instances of vidhop-docker..."
      docker stop "$(docker ps -a -q)"
      docker rm "$(docker ps -a -q)"
      clear
-     docker run --name vidhop-docker -v $PWD/media:/vidhop -v $PWD/vidhop/config/.bash_history:/root/.bash_history -it vidhop-docker /bin/bash
+     if ($vidhop_dir) {
+          docker run --name vidhop-docker -v ${vidhop_dir}:/vidhop -v $PWD/vidhop/.bash_history:/root/.bash_history -it vidhop-docker /bin/bash
+          return
+     }
+     if (-Not (Test-Path $PWD/media) -And -Not (Test-Path $PWD/vidhop/.bash_history)) {
+          Write-Output "could not find /media and vidhop/.bash_history"
+          Write-Output "you are not inside the vidhop-docker directory"
+          Write-Output "navigate to the vidhop-docker directory and try again"
+          return
+     }
+     docker run --name vidhop-docker -v $PWD/media:/vidhop -v $PWD/vidhop/.bash_history:/root/.bash_history -it vidhop-docker /bin/bash
 }
 
 Function Stop-Vidhop() {
@@ -58,4 +66,8 @@ Function BuildNoCache-Vidhop() {
 Function Clear-DockerContainers {
      docker stop $(docker ps -a -q)
      docker rm $(docker ps -a -q)
+}
+
+Function Edit-PsProfile {
+     codium d:\Users\krist\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 }
